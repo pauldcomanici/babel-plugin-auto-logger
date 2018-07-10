8859b45dba5236aeb63cdbc7493134413606e791
darkyndy <darkyndy@gmail.com>
2018-07-10 19:53:46 +0300

    - add type definitions for JSDoc

---
+++ typedef.js
@@ -0,0 +1,48 @@
+/**
+ * Type definition
+ */
+
+/**
+ * Plugin settings for log level
+ *
+ * @typedef {Object} LoggerLevelObj
+ * @property {String} methodName - property name for logger object that is a function and will be called for this log level
+ */
+
+/**
+ * Plugin Settings for levels for logging data
+ *
+ * @typedef {Object} LoggerLevelsObj
+ * @property {LoggerLevelObj} debug - debug level settings
+ * @property {LoggerLevelObj} error - error level settings
+ * @property {LoggerLevelObj} info - info level settings
+ * @property {LoggerLevelObj} log - log level settings
+ * @property {LoggerLevelObj} warn - warn level settings
+ */
+
+/**
+ * Plugin Settings for logging data
+ *
+ * @typedef {Object} LoggerDataObj
+ * @property {String} [source] - logger source, npm package
+ * @property {String} [name='console'] - logger name, name for the import, if is not specified or it is 'console' no import will be made
+ * @property {LoggerLevelsObj} levels - object with log levels
+ */
+
+/**
+ * Plugin Settings
+ *
+ * @typedef {Object} PluginConfigObj
+ * @property {RegExp} sourceMatcher - regular expression for source matching
+ * @property {RegExp} sourceExcludeMatcher - regular expression for exclude source matching
+ * @property {LoggerDataObj} loggingData - object with logging data
+ */
+
+/**
+ * Plugin known data that is sent as parameter
+ *
+ * @typedef {Object} LogResourceObj
+ * @property {String} column - column number where the source code is located
+ * @property {String} line - line number where the source code is located
+ * @property {String} name - name (usually the function name)
+ */
